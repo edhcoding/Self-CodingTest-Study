d731816d676483308b3a0ff91d233abd7ed331f6
eundonghyeok <mephisto7191@naver.com>
2025-01-11 18:39:45 +0900

    11650(좌표 정렬하기)

---
+++ 코테 공부 txt/정렬(sorting)알고리즘/11650(좌표 정렬하기).js
@@ -0,0 +1,15 @@
+// let input = require("fs").readFileSync(0, "utf-8").trim().split("\n");
+
+let input = ["5", "3 4", "1 1", "1 -1", "2 2", "3 3"];
+
+let numbers = input
+  .slice(1)
+  .map((item) => item.split(" "))
+  .sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    return a[0] - b[0];
+  })
+  .map((item) => item.join(" "))
+  .join("\n");
+
+console.log(numbers);
