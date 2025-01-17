467199505a4dc84f0d5bff12aa7e8599417eba30
eundonghyeok <mephisto7191@naver.com>
2025-01-17 18:56:38 +0900

    2512(예산)

---
+++ 코테 공부 txt/이진 탐색(Binary Search) 알고리즘/2512(예산).js
@@ -0,0 +1,27 @@
+// let input = require("fs").readFileSync(0, "utf-8").trim().split("\n");
+
+let input = ["4", "120 110 140 150", "485"]; // 127
+
+let N = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+let M = Number(input[2]);
+
+let start = 1;
+let end = arr.reduce((a, b) => Math.max(a, b));
+let result = 0;
+
+while (start <= end) {
+  let mid = parseInt((start + end) / 2);
+  let total = 0;
+
+  for (let x of arr) total += Math.min(mid, x);
+
+  if (total <= M) {
+    result = mid;
+    start = mid + 1;
+  } else {
+    end = mid - 1;
+  }
+}
+
+console.log(result);
